a53d247775f6c3f8818a29f879fbc006dc26386f
Igor <iKorobyev@google.com>
2020-11-04 01:15:50 +0300

    Completed Categories, SortPopup. Add React-Router, 2 pages: Home and Cart

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 function Categories({ items }) {
-    const [ activeItem, setActiveItem ] = React.useState(null);
+    const [activeItem, setActiveItem] = React.useState(null);
     const onSelectItem = index => {
         setActiveItem(index);
     }
-    console.log(activeItem, setActiveItem);
 
     return <div className="categories">
         <ul>
             <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
-            {items.map((item, index) => <li
+            {items && items.map((item, index) => <li
                 className={activeItem === index ? 'active' : ''}
                 key={`${item}_${index}`}
                 onClick={() => onSelectItem(index)}>
