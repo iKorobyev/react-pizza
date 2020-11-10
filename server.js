3074be876e762750b90a45f0a438f688cf26e233
Igor <iKorobyev@google.com>
2020-11-11 01:13:48 +0300

    Deploy update 3

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-    static: './build',
+  static: './build',
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(PORT, () => {
-    console.log('Server is running');
+  console.log('Server is running');
 });
